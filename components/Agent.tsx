ed33b685ab068cfc3aa6640e226c6a063f122ed9
SWASTIK-WD <swastikranjanmandal@gmail.com>
2025-04-24 19:40:04 +0530

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -82,10 +82,11 @@ const Agent = ({
   const handleGenerateFeedback = async (messages: SavedMessage[]) => {
     console.log('Generate Feedback Here');
 
-    const { success,id } ={
-      success:true,
-      id: 'feedback-id'
-    }
+    const { success,feedbackId:id } = await createFeedback({
+      interviewId : interviewId,
+      userId : userId,
+      transcript : messages
+    })
 
     if(success && id){
       router.push(`/interview/${interviewId}/feedback`)
